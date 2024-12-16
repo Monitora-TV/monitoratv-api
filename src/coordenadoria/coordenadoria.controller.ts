901b6e238d942cf67509d56cc5e306a15ad42732
gustavokanashiro <gustavo_kanashiro@hotmail.com>
2024-12-16 18:41:07 -0300

    feat coordenadoria

--- src/coordenadoria/coordenadoria.controller.ts
+++ src/coordenadoria/coordenadoria.controller.ts
@@ -1,20 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { JwtGuard } from './../auth/auth/jwt.guard'; // Guard de Autenticação JWT
+import { TenantGuard } from './../tenant/tenant.guard'; // Guard de Tenant (verificação de acesso)
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'; // Para a documentação OpenAPI
 import { CoordenadoriaService } from './coordenadoria.service';
 import { CreateCoordenadoriaDto } from './dto/create-coordenadoria.dto';
 import { UpdateCoordenadoriaDto } from './dto/update-coordenadoria.dto';
 
+
+
+@ApiTags('Coordenadorias de saúde') // Define o grupo de tags para a documentação OpenAPI
 @Controller('coordenadoria')
 export class CoordenadoriaController {
   constructor(private readonly coordenadoriaService: CoordenadoriaService) {}
 
+  // Rota para criar um novo registro de coordenadoria
+  @UseGuards(JwtGuard, TenantGuard) // Protege com JwtGuard e TenantGuard
   @Post()
+  @ApiOperation({ summary: 'Criar um novo registro de coordenadoria' })
+  @ApiResponse({ status: 201, description: 'Registro de coordenadoria criado com sucesso.' })
   create(@Body() createCoordenadoriaDto: CreateCoordenadoriaDto) {
     return this.coordenadoriaService.create(createCoordenadoriaDto);
   }
 
+  // Rota para listar todos os registros de coordenadoria
+  @UseGuards(JwtGuard, TenantGuard) // Protege com JwtGuard e TenantGuard
   @Get()
-  findAll() {
-    return this.coordenadoriaService.findAll();
+  @ApiOperation({ summary: 'Listar todos os registros de coordenadoria' })
+  @ApiResponse({ status: 200, description: 'Lista de coordenadoria retornada.' })
+  async findAll() {
+    return await this.coordenadoriaService.findAll();
   }
 
   @Get(':id')
--- src/coordenadoria/coordenadoria.module.ts
+++ src/coordenadoria/coordenadoria.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoordenadoriaService } from './coordenadoria.service';
 import { CoordenadoriaController } from './coordenadoria.controller';
+import { DatabaseModule } from 'src/database/database.module'; // Conexão com o banco de dados
+
 
 @Module({
+  imports: [DatabaseModule], // Importa o módulo do banco de dados
   controllers: [CoordenadoriaController],
   providers: [CoordenadoriaService],
 })
